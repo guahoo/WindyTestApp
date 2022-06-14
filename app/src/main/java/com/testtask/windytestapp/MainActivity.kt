40b52d677c824ad8b88d9343c72dd4b838aab594
Guahoo <ilylyubov@gmail.com>
2022-06-14 11:51:48 +0300

    Initial commit

---
+++ app/src/main/java/com/testtask/windytestapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.testtask.windytestapp
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+         initUI()
+    }
+
+    private fun initUI() {
+        btn_calc_result.setOnClickListener {
+            GlobalScope.launch {
+                if (et_input_it.text.toString()!=""){
+                    numbersFlow(et_input_it.text.toString().toInt()).sum { it }
+                } else {
+                    Toast.makeText(applicationContext, "Пожалуйста, введите число!", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+    }
+
+
+    private fun numbersFlow(intCount:Int): Flow<Int> = flow {
+        for (i in 1..intCount) {
+            delay(100)
+            emit(i)
+        }
+    }
+
+    suspend fun <T> Flow<T>.sum(intFlow: suspend (T) -> Int): Int {
+        var sum = 0
+        onEach {
+            sum += intFlow(it)
+            tv_show_result.text = sum.toString()
+        }.collect()
+        return sum
+    }
+}
